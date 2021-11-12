4f42201c8a142e69b51d865d402b1010b9de8832
Lokesh-Kanna <lokeshkanna.work@gmail.com>
2021-11-12 15:08:38 +0530

    Added Navbar and home intro

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import MenuIcon from '@mui/icons-material/Menu';
+import InputLabel from '@mui/material/InputLabel';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Intro />
     </div>
   );
+}
+
+function Navbar() {
+  return (
+    <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" ClassName="nav-cont" component="div" sx={{ flexGrow: 1 }}>
+            Shopping Cart
+          </Typography>
+          <Typography variant="h6" ClassName="nav-cont" component="div" sx={{ flexGrow: 1 }}>
+            Home
+          </Typography>
+          <Typography variant="h6" ClassName="nav-cont" component="div" sx={{ flexGrow: 1 }}>
+            About
+          </Typography>
+          <InputLabel id="demo-simple-select-label">Shop</InputLabel>
+          <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          label="Age"
+        >
+          <MenuItem>All Products</MenuItem>
+          <MenuItem>Popular Items</MenuItem>
+          <MenuItem>New Arrivals</MenuItem>
+        </Select>
+        </Toolbar>
+      </AppBar>
+  )
+}
+
+function Intro() {
+  return (
+    <div id="intro-div">
+      <p id="intro-title">Shop in Style <br></br> <span id="intro-span">With this shop homepage template</span></p> 
+      <p></p>
+    </div>
+  )
 }
 
 export default App;
